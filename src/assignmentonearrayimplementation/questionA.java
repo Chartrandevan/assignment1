6c411d42efaf42715106a1d226014ee088b6418a
chartrand-e35 <chartrand-e35@1W07-09.uwds.uwinnipeg.ca>
2017-11-28 11:16:13 -0600

    finished product

--- src/assignmentonearrayimplementation/questionA.java
+++ src/assignmentonearrayimplementation/questionA.java
@@ -1,4 +1,4 @@
-package assignmentonearrayimplementation;
+package assignment1arrayimplementation;
 
 /**
  * ******************************************************************
@@ -22,7 +22,7 @@ import java.util.Random;
 
 public class questionA {  // begin class
 
-    public static void main(String[] args) throws IOException {  // begin main
+    public static  void main(String[] args)throws IOException {  // begin main
 
         // ********* declaration of constants **********
         // ********** declaration of variables **********
@@ -31,17 +31,20 @@ public class questionA {  // begin class
 
         String banner;                          //string for printing banners
         String prompt;				// prompt for use in input dialogs
-
-        String delim = "[ :]+";		// delimiter string for splitting input string
+        
+        String tokens[];
+        String delim = " ";		// delimiter string for splitting input string
         String tabSpace = "      ";	// six spaces
 
         int max;
         int count;
-
+        int countdracula = 0;
+        int mastercount;
+        
         // create instances of objects for i/o and formatting
         //ConsoleReader console = new ConsoleReader(System.in);
         //DecimalFormat df1 = new DecimalFormat("$##.00");
-        //BufferedReader fin = new BufferedReader(new FileReader("name of file"));
+        BufferedReader fin = new BufferedReader(new FileReader("testOut.txt"));
         PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("testOut.txt")));
 
         // ********** Print output Banner **********
@@ -52,34 +55,124 @@ public class questionA {  // begin class
         JOptionPane.showMessageDialog(null, banner);
 
         // ************************ get input **********************
-        prompt = "Enter a number. \n\n";
+        prompt = "Enter the number whose \nfrequency you would like to know.\n To skip enter 0 \n\n";
 
         // get input from the keyboard by calling the getNumber method
         // note the argument that is passed into the method
         // also note that since getNumber has a return type (int) you must use it
         // in a context where the returning value has someplace to go
         // ************************ processing ***************************
+        try{
         Random rand = new Random();
         max = rand.nextInt(50) + 951;
         //  sum = add(op1, op2);            // add operands 1 and 2
         int[] arreh = new int[max];
+        int[] echo = new int[max];
+        int[]freq = new int[100];
         for (count = 0; count < max; count++) {
             arreh[count] = rand.nextInt(100) + 1;
         }//end for
+          
+         printarray(arreh, max, fout);
+        fout.close();	// close output buffer
+        
 
+          // prime loop
+         count = 0;
+           strin = fin.readLine();    
+          
+           while(strin != null){
+           echo[count] = Integer.parseInt(strin);
+           count ++;
+           strin = fin.readLine(); 
+           }//end while
+           
+          for(mastercount = 0; mastercount < 100; mastercount++){         
+           
+              count = 0;    
+           while(count < max){
+               
+           if(echo[count] == mastercount + 1){
+           freq[mastercount]++;
+           }//end if
+           
+               count ++;
+               
+           }//end while
+          }//end for
+          
+          int n = getNumber(banner, prompt);
+          delim = "-";
+          
         // ************************ print output ****************************
-        System.out.println(printarray(arreh, max));
-        System.out.println(count);
-        fout.println(printarray(arreh, max));
+        
+        if(n > 0){
+       System.out.println("frequency of "+ n + " = " + freq[n-1]);
+        }//end if
+        
+        strin = JOptionPane.showInputDialog(banner, "enter the range of numbers you want. eg: 10-20\n enter 0 to skip.");
+          tokens = strin.split(delim);
+          n = Integer.parseInt(tokens[0]);
+          int i = Integer.parseInt(tokens[1]);
+        if(n > 0 && i > 0){
+            for (n = n; n<=i; n++ ){
+              System.out.print("number " + n + " = " + freq[n-1]+ ", ");  
+            }//end for
+        }//end if
+        
+        
+        System.out.println("\nHighest frequency " + bignumarray(freq,100));
+         System.out.println("Highest frequency " + smallnumarray(freq,100));
+        // printarray(echo, max);
+       System.out.println("Length of array " + count);
+       count = 0;
+       prompt = "see all the numbers of a certain frequency. enter -1 to skip.";
+      n = getNumber(banner,prompt);
+       while(count < 100){
+        if(freq[count] == n) {
+        System.out.println(count + 1 +" has a frequency of " + n);  
+        }//end if
+          count ++;
+       }//end while 
+       
+        
         // ******** closing message *********
         System.out.println("end of processing.");
 
         // ***** close streams *****
-        //fin.close();			// close input buffer
-        fout.close();			// close output buffer
+        fin.close();			// close input buffer
+        }//end try
+        catch(ArrayIndexOutOfBoundsException e){
+         JOptionPane.showMessageDialog(null,"Enter the correct amount of numbers","Try again", JOptionPane.ERROR_MESSAGE);
+     }//end catch
     }  // end main
 
 // ******************** static methods *****************************
+    
+    //************************************************
+    // Purpose: load an array from disk
+    // Interface: IN: inputfile buffer
+    // Returns: none
+    // *****************************************************
+    public static int loadsimplearray(int[] list, BufferedReader fin) throws IOException {
+        String strin = fin.readLine();     // prime loop
+        int count = 0;
+        
+        while (strin != null){
+            
+            list[count] = Integer.parseInt(strin);
+            count++;               // count actual array elements
+
+            //System.out.println(strin);
+            strin = fin.readLine();
+        } // end eof
+        
+       fin.close();
+        return count;
+    }//end load list
+
+    
+
     //************************************************
     // Purpose: to find the smallest number in an array
     // Interface: IN: the array and length of it
@@ -120,23 +213,25 @@ public class questionA {  // begin class
     // Interface: IN: the array and length of it
     // Returns: array as a string
     // *****************************************************
-    public static String printarray(int[] arreh, int max) {
-        String strin = "";
-        int scount = 1;
+    public static void printarray(int[] arreh, int max, PrintWriter fout) {
         for (int count = 0; count < max; count++) {
-            strin += arreh[count];
-            strin += " ";
-            if (scount % 10 == 0) {
-                strin += "\n";
-            }//end if 
-
-            scount++;
-
-        } //end for 
-
-        return strin;
+            fout.println(arreh[count]);
+        } //end for     
+        fout.close();
     }//end printarray
 
+    //************************************************
+    // Purpose: to print an array
+    // Interface: IN: the array and length of it
+    // Returns: array as a string
+    // *****************************************************
+    public static void printarray(int[] arreh, int max) {
+        for (int count = 0; count < max; count++) {
+            System.out.println(arreh[count]);
+        } //end for 
+
+    }//end printarray
+    
     //************************************************
     // Purpose: to add an array
     // Interface: IN: the array and length of it
@@ -165,7 +260,7 @@ public class questionA {  // begin class
     // Interface: IN: string prompt
     // Returns: int read from keyboard buffer
     // *****************************************************
-    public static int getNumber(String p, String banner, String promt) {
+    public static int getNumber(String banner, String promt) {
         int n = 0;          // local varible to hold integer from keyboard buffer
         n = Integer.parseInt(JOptionPane.showInputDialog(banner, promt));
         return (n);
@@ -219,3 +314,4 @@ public class questionA {  // begin class
         return banner;
     } // end windowBanner
 }  // end clas
+
